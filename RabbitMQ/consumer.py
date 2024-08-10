fa1e4b7579aaff7bb62fbe6778c4d70de59e63cc
ursmaheshj <omkar28399@gmail.com>
2024-08-10 14:11:30 +0530

    added consumer code to consume messages

---
+++ RabbitMQ/consumer.py
@@ -0,0 +1,20 @@
+import pika
+
+def on_message_receive(ch, method, properties, body):
+    print(f"New Message Received: {body}")
+
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='letterbox')
+
+channel.basic_consume(queue='letterbox',auto_ack=True,
+                      on_message_callback=on_message_receive)
+
+
+print("start consuming")
+channel.start_consuming()
--- RabbitMQ/producer.py
+++ RabbitMQ/producer.py
@@ -1,6 +1,6 @@
 import pika
 
-connection_parameter = pika.ConnectionParameters('http://localhost:5672/')
+connection_parameter = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameter)
 
