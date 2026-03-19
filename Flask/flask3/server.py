a80569babe14853053188499251689270455d447
Mahesh Jadhav <omkar28399@gmail.com>
2026-03-19 20:50:56 +0530

    templating and request methods with form data added

---
+++ Flask/flask3/server.py
@@ -0,0 +1,23 @@
+from flask import Flask,redirect,url_for,render_template,request
+
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('index.html',var1=1,var2="ram",var="dsf")
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        name = request.form.get('Name','ram')
+        return redirect(url_for('userprofile',name=name))
+    return render_template('login.html')
+
+@app.route('/<name>')
+def userprofile(name):
+    return f"My name is {name}"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
